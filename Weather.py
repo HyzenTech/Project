7ed9a6814eee1f7bde703f87a8033991cc7b4c7f
HyzenTech <88046906+HyzenTech@users.noreply.github.com>
2021-07-27 22:32:37 +0700

    Update Weather.py

--- Weather.py
+++ Weather.py
@@ -2,7 +2,7 @@ import requests
 #import os
 from datetime import datetime
 
-api_key = '87d845b0b6cf29baa1a73cc34b067a95'
+api_key = '585a96b22596d799dfa331820ff4424d'
 location = input("Enter the city name: ")
 
 complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
